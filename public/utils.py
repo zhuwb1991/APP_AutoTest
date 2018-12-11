5521859f8cbebe62ddf1e680fd7648d29b8afd6b
zhuwb1991 <1032988752@qq.com>
2018-12-11 15:36:19 +0800

    新增写文件方法

--- public/utils.py
+++ public/utils.py
@@ -10,6 +10,26 @@ def get_yaml(path):
         print("未找到文件:" + path)
 
 
+def write_file(filename, content, cover=False):
+
+    try:
+        newstr = ''
+        if isinstance(content, list or tuple):
+            for item in content:
+                newstr = newstr + item + '\n'
+        else:
+            newstr = content
+        if cover:
+            # 覆盖文件内容
+            mode = 'w'
+        else:
+            mode = 'a'
+        with open(filename, mode) as f:
+            f.write(newstr)
+    except Exception as e:
+        print(e)
+
+
 if __name__ == "__main__":
     import os
     PATH = lambda p: os.path.abspath(
